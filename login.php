f35c871a73d4816e829352247ec702574ff01450
Daichi0119110 <daichi0119110@gmail.com>
2015-05-26 14:00:05 +0900

    login.phpを変更

--- login.php
+++ login.php
@@ -1,78 +1,25 @@
 <?php
 
-require_once('config.php');
-require_once('function.php');
-
 session_start();
 
-function getUser($email, $password, $dbh){
-	$sql = "select * from users where email = :email and password = :password limit 1";
-	$stmt = $dbh->prepare($sql);
-	$params = array(
-		":email" => $email,
-		":password" => getSha1Password($password)
-		);
-	$stmt->execute($params);
-	$user = $stmt->fetch();
-	return $user ? $user : false;
-}
+require_once('config.php');
+require_once('function.php');
+require_once("facebook.php");
 
+//facebookログイン用
+$config = array(
+    'appId'  => '1588772488021580',
+    'secret' => '0049bd6e09b21a3fd257461f7b675ec2'
+);
+$facebook = new Facebook($config);
 
-
-//ログインしていたら元に戻る
-if($_SESSION['me']) {
-	header("Location:".SITE_URL);
-	exit;
-} 
-
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-	//POSTじゃなかったら
-	setToken();
-
-} else {
-	//POSTだったら
-	checkToken();
-
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-
-	$dbh = connectDB();
-
-	$err = array();
-
-	//メールアドレスの形式がおかしい
-	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-		$err['email'] = 'メールアドレスが正しくないです';
-	}
-
-	//メールアドレスが空
-	if (!$email){
-		$err['email'] = '入力してください';
-	}
-
-	//パスワードが空
-	if (!$password){
-		$err['password'] = '入力してください';
-	}
-
-	//メールアドレスが存在しない
-	if(!emailExist($email,$dbh)) {
-		$err['email'] = 'このメールアドレスは登録されていません';
-	}
-
-	//パスワードが正しくない
-	if (!$me = getUser($email, $password, $dbh)){
-		$err['password'] = 'パスワードが一致しません';
-	}
-
-	if(empty($err)){
-		//セッションハイジャック対策
-		session_regenerate_id();
-
-		//ログイン処理
-		$_SESSION['me'] = $me;
-		header('Location:'.SITE_URL);
-		exit;
+if ($facebook->getUser()) {
+	try {
+		$user = $facebook->api('/me','GET');
+	} catch(FacebookApiException $e) {
+		//取得に失敗したら例外をキャッチしてエラーログに出力
+		error_log($e->getType());
+		error_log($e->getMessage());
 	}
 }
 
@@ -90,15 +37,26 @@ Head($_SESSION['me']['username']);
 <body>
 
 	<h1>ログイン</h1>
-	<form action="" method="POST">
-		<p>メールアドレス：<input type="text" name="email" value="<?php echo h($email); ?>"><?php echo h($err['email']); ?></p>
-		<p>パスワード：<input type="password" name="password" value="<?php echo h($password); ?>"><?php echo h($err['password']); ?></p>
-		<input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
-		<p><input type="submit" value='ログイン'></p>
-	</form>
+<?php
+		if (isset($user)) {
+			//ログイン済みでユーザー情報が取れていれば表示
+			echo '<pre>';
+			print_r($user);
+			echo '</pre>';
 
+			//ログアウト処理
+			$logoutUrl = $facebook->getLogoutUrl();
+		    //ユーザ情報を取得
+		    $user_info = $facebook->getUser();
+		    //ログアウト用のリンクを出力
+		    echo "<h1><a href='".$logoutUrl."'>ログアウト</a></h1>";
+
+		} else {
+			//未ログインならログイン URL を取得してリンクを出力
+			$loginUrl = $facebook->getLoginUrl();
+			echo '<h1><a href="' . $loginUrl . '">Login with Facebook</a></h1>';
+		}
+	?>
 </body>
 </html>
-
-
 
